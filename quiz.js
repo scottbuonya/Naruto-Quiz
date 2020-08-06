fe5a67f58e3b1a26212793f7a174bc31587a9d18
Scott Buonya <scottbuonya@gmail.com>
2020-08-05 21:31:44 -0400

    added questions

--- quiz.js
+++ quiz.js
@@ -1,9 +1,7 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-
+(function(){
 function buildQuiz() {
     const output = [];
+
     myQuestions.forEach(
         (currentQuestion, questionNumber) => {
             const answers = [];
@@ -16,16 +14,74 @@ function buildQuiz() {
                     </label>`
                 );
             }
+            output.push(
+                <div class="slide">
+                    <div class="question"> ${currentQuestion.question} </div>
+                    <div class="answers"> ${answers.join("")} </div>
+                </div>   
+            );
         }
-    )
+    );
+    quizContainer.innerHTML = output.join("");
 }
 
-function showResults() {}
+function showResults() {
+    const answersContainers = quizContainer.querySelectorAll(".answers");
 
-buildQuiz();
+    let numCorrect = 0;
 
-submitButton.addEventListener("click" , showResults);
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
 
+        const answersContainer = answersContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answersContainer.querySelector(selector) || {}).value;
+
+
+        if(userAnswer === currentQuestion.correctAnswer){
+            numCorrect++;
+
+            answersContainers[questionNumber].getElementsByClassName.color = "lightgreen";
+        }
+        else{
+            answersContainers[questionNumber].getElementsByClassName.color = "red";
+        }
+    });
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove("active-slide");
+    slides[n].classList.add("active-slide");
+    currentSlide = n;
+    if(currentSlide === 0){
+        previousButton.style.display = "none";
+    }
+    else{
+        previousButton.style.display = "inline-block";
+    }
+    if(currentSlide === slides.length-1){
+        nextButton.style.display = "none";
+        submitButton.style.display = "inline-block";
+    }
+    else{
+        nextButton.style.display = "inline-block";
+        submitButton.style.display = "none";
+    }
+}
+
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function showPreviousSlide() {
+    showSlide(currentSlide -1);
+}
+
+
+
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
 const myQuestions = [
     {
         Question: "Who is the main character of Naruto?",
@@ -78,6 +134,18 @@ const myQuestions = [
     },
 ];
 
+buildQuiz();
+
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+
+showSlide(currentSlide);
+
+submitButton.addEventListener("click", showResults);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
+})();
 
 
 
