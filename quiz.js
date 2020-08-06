0feb3557e24c5735c1aa98926f64ee39c26d1585
Scott Buonya <scottbuonya@gmail.com>
2020-08-06 16:41:47 -0400

    added an auto timer

--- quiz.js
+++ quiz.js
@@ -145,3 +145,28 @@
     
   })();
 
+  function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        } else (timer === 0) {
+          clearInterval();
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var oneMinutes = 60 * 1,
+        display = document.querySelector('#time');
+    startTimer(oneMinutes, display);
+};
+
